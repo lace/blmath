770ef6a7429c9e96cae3bcd6ed9ccf4166333159
Alex Weiss <algrs@cacography.net>
2017-02-13 12:13:55 -0500

    Fix bit rot

--- blmath/geometry/apex.py
+++ blmath/geometry/apex.py
@@ -11,6 +11,38 @@ def apex(points, axis):
     coords_on_axis = points.dot(axis)
     return points[np.argmax(coords_on_axis)]
 
+def inflection_points(points, axis, span):
+    '''
+    Find the list of vertices that preceed inflection points in a curve. The curve is differentiated
+    with respect to the coordinate system defined by axis and span.
+
+    axis: A vector representing the vertical axis of the coordinate system.
+    span: A vector representing the the horiztonal axis of the coordinate system.
+
+    returns: a list of points in space corresponding to the vertices that
+    immediately preceed inflection points in the curve
+    '''
+
+    coords_on_span = points.dot(span)
+    dx = np.gradient(coords_on_span)
+    coords_on_axis = points.dot(axis)
+
+    # Take the second order finite difference of the curve with respect to the
+    # defined coordinate system
+    finite_difference_2 = np.gradient(np.gradient(coords_on_axis, dx), dx)
+
+    # Compare the product of all neighboring pairs of points in the second derivative
+    # If a pair of points has a negative product, then the second derivative changes sign
+    # at one of those points, signalling an inflection point
+    is_inflection_point = [finite_difference_2[i] * finite_difference_2[i + 1] <= 0 for i in range(len(finite_difference_2) - 1)]
+
+    inflection_point_indices = [i for i, b in enumerate(is_inflection_point) if b]
+
+    if len(inflection_point_indices) == 0:
+        return []
+
+    return points[inflection_point_indices]
+
 def farthest(from_point, to_points):
     '''
     Find the farthest point among the inputs, to the given point.
