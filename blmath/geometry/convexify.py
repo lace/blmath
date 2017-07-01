a9e72aee9567f2907962b77bf1fc59484c45dcf1
EERac <eric@bodylabs.com>
2017-06-30 21:56:45 -0400

    refactored convexify_planar_curve
    convexify_planar_curve now flattens points without needing to explicitly rotate or translate them. An optional normal can also be passed in to save time

--- blmath/geometry/convexify.py
+++ blmath/geometry/convexify.py
@@ -1,4 +1,4 @@
-def convexify_planar_curve(polyline, flatten=False, want_vertices=False):
+def convexify_planar_curve(polyline, flatten=False, want_vertices=False, normal=None):
     '''
     Take the convex hull of an almost planar 2-D curve in three-space.
 
@@ -15,31 +15,24 @@ def convexify_planar_curve(polyline, flatten=False, want_vertices=False):
     import numpy as np
     from scipy.spatial import ConvexHull  # First thought this warning was caused by a pythonpath problem, but it seems more likely that the warning is caused by scipy import hackery. pylint: disable=no-name-in-module
     from blmath.geometry import Polyline
-    from blmath.geometry.transform.rotation import rotate_to_xz_plane
-    from blmath.geometry.transform.translation import translation
+    from blmath.geometry.transform.rotation import estimate_normal
 
-    if len(polyline.v) <= 1:
+    v = polyline.v
+    if len(v) <= 1:
         return polyline
 
-    rotated, R, p0 = rotate_to_xz_plane(polyline.v)
+    if normal is None:
+        normal = estimate_normal(v)
 
-    if flatten:
-        rotated[:, 1] = np.mean(rotated[:, 1])
+    # to call ConvexHull, the points must be projected to a plane.
+    # with an eye toward numerical stability, this can be done by dropping 
+    # whichever dimension is closest to the normal
+    dim_to_drop = np.argmax(np.abs(normal))
+    dims_to_keep = [i for i in range(3) if i != dim_to_drop]
+    proj_v = v[:, dims_to_keep]
 
-    projected = rotated[:, [0, 2]]
-
-    hull_vertices = ConvexHull(projected).vertices
-    rotated_hull = rotated[hull_vertices]
-
-    R_inv = np.array(np.mat(R.T).I)
-    inv_rotated = np.dot(rotated_hull, R_inv)
-
-    if p0 is not None:
-        curve_hull = translation(np.array(inv_rotated), -p0)[0] # pylint: disable=invalid-unary-operand-type
-    else:
-        curve_hull = inv_rotated
-
-    result = Polyline(v=curve_hull, closed=polyline.closed)
+    hull_vertices = ConvexHull(proj_v).vertices
+    result = Polyline(v=v[hull_vertices], closed=polyline.closed)
 
     if want_vertices:
         return result, hull_vertices
