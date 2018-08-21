6cba56fbf154217d731edf6d8f10197a64939b11
Martin Kristiansen <lille.kemiker@gmail.com>
2018-08-21 12:27:05 -0400

    Updating pip import to work with version 10

--- setup.py
+++ setup.py
@@ -9,7 +9,12 @@ except (IOError, ImportError):
 
 import importlib
 import platform
-from pip.req import parse_requirements
+
+try:  # pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError:  # pip <= 9.0.3
+    from pip.req import parse_requirements
+
 from setuptools import setup, Extension
 import numpy as np
 
